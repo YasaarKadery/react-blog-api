1b2cdf48f4b38d90b91afff28609c5467fca8f1c
YasaarKadery <ykadery12@gmail.com>
2023-06-01 11:00:05 -0400

    refactored some code

--- main.go
+++ main.go
@@ -20,9 +20,12 @@ type Post struct {
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 	ImageSrc  string `json:"image_src"`
+	Markdown  string `json:"markdown"`
 }
 
-var db *sql.DB
+type application struct {
+	db *sql.DB
+}
 
 func main() {
 	cfg := mysql.Config{
@@ -34,8 +37,7 @@ func main() {
 		AllowNativePasswords: true,
 	}
 
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
+	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,40 +48,44 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	app := application{
+		db: db,
+	}
 
 	router := httprouter.New()
 	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// If "OPTIONS" request, respond immediately
 		if r.Header.Get("Access-Control-Request-Method") != "" {
-			enableCors(&w)
+			app.enableCors(&w)
 			return
 		}
 	})
-	router.GET("/posts", getPosts)
-	router.POST("/posts", createPost)
-	router.GET("/posts/:id", getPost)
-	router.PUT("/posts/:id", updatePost)
-	router.DELETE("/posts/:id", deletePost)
+
+	router.GET("/posts", app.getPosts)
+	router.POST("/posts", app.createPost)
+	router.GET("/posts/:id", app.getPost)
+	router.PUT("/posts/:id", app.updatePost)
+	router.DELETE("/posts/:id", app.deletePost)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func enableCors(w *http.ResponseWriter) {
+func (app *application) enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-func getPosts(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	enableCors(&w)
+func (app *application) getPosts(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	app.enableCors(&w)
 	var posts []Post
-	result, err := db.Query("SELECT id, title, content, created_at,updated_at, image_src from posts")
+	result, err := app.db.Query("SELECT id, title, content, created_at,updated_at, image_src, markdown from posts")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer result.Close()
 	for result.Next() {
 		var post Post
-		err := result.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt, &post.ImageSrc)
+		err := result.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt, &post.ImageSrc, &post.Markdown)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -88,15 +94,15 @@ func getPosts(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	json.NewEncoder(w).Encode(posts)
 }
 
-func createPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	enableCors(&w)
+func (app *application) createPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	app.enableCors(&w)
 	var post Post
 	_ = json.NewDecoder(r.Body).Decode(&post)
-	stmt, err := db.Prepare("INSERT INTO posts(Title,Content,image_src) VALUES(?,?,?)")
+	stmt, err := app.db.Prepare("INSERT INTO posts(Title,Content,image_src, markdown) VALUES(?,?,?,?)")
 	if err != nil {
 		panic(err.Error())
 	}
-	res, err := stmt.Exec(post.Title, post.Content, post.ImageSrc)
+	res, err := stmt.Exec(post.Title, post.Content, post.ImageSrc, post.Markdown)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -108,17 +114,17 @@ func createPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	json.NewEncoder(w).Encode(post)
 }
 
-func getPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	enableCors(&w)
+func (app *application) getPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	app.enableCors(&w)
 	id, _ := strconv.Atoi(ps.ByName("id"))
-	result, err := db.Query("SELECT id, title, content, created_at, updated_at, image_src FROM posts WHERE id = ?", id)
+	result, err := app.db.Query("SELECT id, title, content, created_at, updated_at, image_src, markdown FROM posts WHERE id = ?", id)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer result.Close()
 	var post Post
 	for result.Next() {
-		err := result.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt, &post.ImageSrc)
+		err := result.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt, &post.ImageSrc, &post.Markdown)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -126,26 +132,26 @@ func getPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	json.NewEncoder(w).Encode(post)
 }
 
-func updatePost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	enableCors(&w)
+func (app *application) updatePost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	app.enableCors(&w)
 	id, _ := strconv.Atoi(ps.ByName("id"))
 	var post Post
 	_ = json.NewDecoder(r.Body).Decode(&post)
-	stmt, err := db.Prepare("UPDATE posts SET title = ?, content = ?, image_src= ?, WHERE id = ?")
+	stmt, err := app.db.Prepare("UPDATE posts SET title = ?, content = ?, image_src= ?,markdown=?, WHERE id = ?")
 	if err != nil {
 		panic(err.Error())
 	}
-	_, err = stmt.Exec(post.Title, post.Content, id)
+	_, err = stmt.Exec(post.Title, post.Content, post.ImageSrc, post.Markdown, id)
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Fprintf(w, "Post with ID = %d was updated", id)
 }
 
-func deletePost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	enableCors(&w)
+func (app *application) deletePost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	app.enableCors(&w)
 	id, _ := strconv.Atoi(ps.ByName("id"))
-	stmt, err := db.Prepare("DELETE FROM posts WHERE id = ?")
+	stmt, err := app.db.Prepare("DELETE FROM posts WHERE id = ?")
 	if err != nil {
 		panic(err.Error())
 	}
