f9f1ffba0219ba3a6a3729fb111f1a3dc2e2c024
YasaarKadery <ykadery12@gmail.com>
2023-06-07 12:33:52 -0400

    added proper api endpoints

--- main.go
+++ main.go
@@ -67,8 +67,8 @@ func main() {
 }
 
 func (app *application) enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Origin", "https://yasaarkadery.com")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
